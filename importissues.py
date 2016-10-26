5827b167ec65f47bcd7fd6f43d13ee815ec98e9b
bhoff <bruce.hoff@sagebase.org>
2016-10-26 08:49:20 -0700

    temporary change to capture lost threads

--- importissues.py
+++ importissues.py
@@ -21,6 +21,29 @@ def log(s):
     print s
 
 def main(argv=None):
+    
+    lost=[907, 908,  913, 916, 918,    923,    928,    934,    937,    939,
+    942,
+    951,
+    953,
+    963,
+    966,
+    970,
+    974,
+    976,
+    977,
+    978,
+    980,
+    981,
+    990,
+    997,
+    1004,
+    1017,
+    1022,
+    1024,
+    1029,    1032,    1037,    1041,    1043,    1045,    1047,    1048,
+    1051,    1054,    1067,    1068,    1069,    1081,    1084,    1101,    1114,    1125,    1127, 1128]
+
     config = ConfigParser.ConfigParser()
     config.read(argv[1])
     
@@ -51,10 +74,10 @@ def main(argv=None):
         totalNumberOfResults=int(threads.get('totalNumberOfResults'))
         for thread in threads.get('results'):
             threadid=int(thread.get('id'))
-            if threadid<=lastid:
+            if threadid<=lastid and not threadid in lost:
                 continue
              
-            repository.create_issue(title=thread.get('title'), body='https://www.synapse.org/#!Synapse:'+projectId+'/discussion/threadId='+str(thread.get('id')), labels=['discussion forum'])
+            # repository.create_issue(title=thread.get('title'), body='https://www.synapse.org/#!Synapse:'+projectId+'/discussion/threadId='+str(thread.get('id')), labels=['discussion forum'])
              
             log("Created issue for "+thread.get('id')+" "+thread.get('title'))
             lastid=threadid
